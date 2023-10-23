1e05d2b2e9428975dc39854e84324b8d80838c69
Akira-13 <134654836+Akira-13@users.noreply.github.com>
2023-10-23 16:42:09 -0500

    Update Total_caracters.java

--- Total_caracters.java
+++ Total_caracters.java
@@ -4,6 +4,6 @@ public interface Total_caracters {
     int TOTALDIGITS = 10;
     int TOTALLOWER = 27 ;
     int TOTALUPPER = 27;
-    int TOTALSPEC = 193;
+    int TOTALSPEC = 33;
     double BASE2 = Math.log(2.0);
 }
