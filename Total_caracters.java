cbd9d61ad2ea177730cd518af4a4e805f7f701cb
bxcowo <134749977+bxcowo@users.noreply.github.com>
2023-10-23 13:22:20 -0500

    Add files via upload

---
+++ Total_caracters.java
@@ -0,0 +1,9 @@
+package arquitectura_proyect;
+
+public interface Total_caracters {
+    int TOTALDIGITS = 10;
+    int TOTALLOWER = 26 ;
+    int TOTALUPPER = 26;
+    int TOTALSPEC = 193;
+    double BASE2 = Math.log(2.0);
+}
