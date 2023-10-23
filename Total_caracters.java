7634371290ce95f96757ff6626f27efe43aa4917
Akira-13 <134654836+Akira-13@users.noreply.github.com>
2023-10-23 15:06:19 -0500

    Update Total_caracters.java english to spanish keyboard constants

--- Total_caracters.java
+++ Total_caracters.java
@@ -2,8 +2,8 @@ package arquitectura_proyect;
 
 public interface Total_caracters {
     int TOTALDIGITS = 10;
-    int TOTALLOWER = 26 ;
-    int TOTALUPPER = 26;
+    int TOTALLOWER = 27 ;
+    int TOTALUPPER = 27;
     int TOTALSPEC = 193;
     double BASE2 = Math.log(2.0);
 }
