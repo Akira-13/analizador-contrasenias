64d6555eae53a4efc0b174a9cc54406bed0ef7a7
bxcowo <134749977+bxcowo@users.noreply.github.com>
2023-10-23 13:24:06 -0500

    Delete Total_caracters.java

--- Total_caracters.java
+++
@@ -1,9 +0,0 @@
-package arquitectura_proyect;
-
-public interface Total_caracters {
-    int TOTALDIGITS = 10;
-    int TOTALLOWER = 26 ;
-    int TOTALUPPER = 26;
-    int TOTALSPEC = 193;
-    double BASE2 = Math.log(2.0);
-}
